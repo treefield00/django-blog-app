a18df6d7995d08d9f1a241b252c0616607030d66
treefield00 <kihara_takashi@me.com>
2023-03-02 00:01:58 +0900

    add category, tag

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from blog.models import Post
+from blog.models import Post, Category, Tag
 
 
 admin.site.register(Post)
+admin.site.register(Category)
+admin.site.register(Tag)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
@@ -8,6 +9,19 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
 
+    def get_queryset(self):
+        posts = super().get_queryset()
+
+        return posts.order_by('-updated_at')
+
 class PostDetailView(DetailView):
     model = Post
     templat_name = 'blog/post_detail.html'
+
+    def get_object(self, queryset=None):
+        post = super().get_object(queryset)
+        # 公開済み or ログイン
+        if post.is_published or self.request.user.is_authenticated:
+            return post
+        else:
+            raise Http404 
