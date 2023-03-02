a2cffc0dcf6774ab52d50f8e6ee448c9f716d466
treefield00 <kihara_takashi@me.com>
2023-03-02 23:43:55 +0900

    add CategoryPostListView

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from blog.views import PostListView, PostDetailView
+from blog.views import PostListView, PostDetailView, CategoryPostListView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('category/<str:slug>/', CategoryPostListView.as_view(), name='category-post-list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,15 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 
-from blog.models import Post 
+from blog.models import Post, Category
 # Create your views here.
 
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
 
     def get_queryset(self):
         posts = super().get_queryset()
@@ -25,3 +27,20 @@ class PostDetailView(DetailView):
             return post
         else:
             raise Http404 
+        
+class CategoryPostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    
+    def get_queryset(self):
+        #トップページでアクセスのあったカテゴリーのURLを変数slugに代入
+        slug = self.kwargs['slug']
+        self.category = get_object_or_404(Category, slug=slug)
+
+        return super().get_queryset().filter(category=self.category)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = self.category
+        return context
